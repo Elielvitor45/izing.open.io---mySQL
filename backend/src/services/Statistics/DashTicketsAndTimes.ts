d91a319a9c225e7acea66ace598f0d1460ffa621
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2023-11-24 16:53:16 -0300

    fix: fixed DashTicketsAndTimes query

--- backend/src/services/Statistics/DashTicketsAndTimes.ts
+++ backend/src/services/Statistics/DashTicketsAndTimes.ts
@@ -3,42 +3,35 @@ import sequelize from "../../database";
 
 interface Request {
   startDate: string;
+  
   endDate: string;
   tenantId: string | number;
 }
 
 const query = `
-  select
-  --dt_referencia,
-  sum(qtd_total_atendimentos) qtd_total_atendimentos,
-  sum(qtd_demanda_ativa) qtd_demanda_ativa,
-  sum(qtd_demanda_receptiva) qtd_demanda_receptiva,
-  coalesce(concat(ROUND(AVG(tma)::decimal,0), 0), 'minutes')::interval TMA,
-  coalesce(concat(ROUND(AVG(tme)::decimal,0), 0), 'minutes')::interval TME,
-  (select count(1)
-  from "Contacts" c where c."tenantId" = :tenantId
-  and date_trunc('day', c."createdAt") between :startDate and :endDate
-  ) new_contacts
-  --ROUND(AVG(tma)::decimal,0) TMA,
-  --ROUND(AVG(tme)::decimal,0) TME
-  from (
-  select
-  date_trunc('month', t."createdAt") dt_referencia,
-  1 qtd_total_atendimentos,
-  case when t."isActiveDemand" is true then 1 else 0 end qtd_demanda_ativa,
-  case when t."isActiveDemand" is not true then 1 else 0 end qtd_demanda_receptiva,
-  t."createdAt",
-  to_timestamp(t."closedAt"/1000) closedAt,
-  to_timestamp(t."startedAttendanceAt"/1000) startedAttendanceAt,
-  extract(epoch from AGE(to_timestamp(t."closedAt"/1000), t."createdAt")::interval)/60 tma,
-  extract(epoch from AGE(to_timestamp(t."startedAttendanceAt"/1000), t."createdAt"::timestamp)::interval)/60 tme,
-  t."tenantId"
-  from "Tickets" t
-  where t."tenantId" = :tenantId
-  and date_trunc('day', t."createdAt") between :startDate and :endDate
-  ) a
-  --group by dt_referencia
-  order by 1 Desc
+SELECT
+SUM(qtd_total_atendimentos) AS qtd_total_atendimentos,
+SUM(qtd_demanda_ativa) AS qtd_demanda_ativa,
+SUM(qtd_demanda_receptiva) AS qtd_demanda_receptiva,
+SEC_TO_TIME(AVG(tma)*60*60) AS TMA,
+SEC_TO_TIME(AVG(tme)*60*60) AS TME, 
+(SELECT COUNT(1)
+ FROM Contacts c 
+ WHERE c.tenantId = tenantId
+ AND DATE(c.createdAt) BETWEEN @startDate AND @endDate
+) AS new_contacts
+FROM (
+SELECT
+  1 AS qtd_total_atendimentos,
+  CASE WHEN t.isActiveDemand = 1 THEN 1 ELSE 0 END AS qtd_demanda_ativa,
+  CASE WHEN t.isActiveDemand != 1 THEN 1 ELSE 0 END AS qtd_demanda_receptiva,
+  TIMESTAMPDIFF(MINUTE, t.createdAt, FROM_UNIXTIME(t.closedAt/1000)) AS tma,
+  TIMESTAMPDIFF(MINUTE, t.createdAt, FROM_UNIXTIME(t.startedAttendanceAt/1000)) AS tme
+FROM Tickets t
+WHERE t.tenantId = tenantId
+AND DATE(t.createdAt) BETWEEN @startDate AND @endDate
+) a
+ORDER BY 1 DESC
 `;
 
 const DashTicketsAndTimes = async ({
